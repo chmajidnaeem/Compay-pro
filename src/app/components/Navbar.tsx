bb1a9c8b78ce5fd09e942bbc2e55f966dddbe059
Hadiqa <hadiqamehmood076@gmail.com>
2024-02-11 20:14:55 +0500

    about us

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -27,14 +27,14 @@ const Navbar = () => {
           <div className="hidden md:block">
             <ul className="flex space-x-8 ">
               <li>
-                <Link href="/about">
+                <Link href="/aboutus">
                   <div className="text-[#FF4802] hover:text-gray-300 py-2 font-semibold">
                     Home
                   </div>
                 </Link>
               </li>
               <li>
-                <Link href="/about">
+                <Link href="/aboutus">
                   <div className="text-[#52525B] hover:text-gray-300 py-2 font-semibold">
                     About Us
                   </div>
