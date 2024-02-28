8175893a42710da5725fddd9ba10330ad362f0d9
AREEBA-MUHAMMAD-AIJAZ <96327834+AREEBA-MUHAMMAD-AIJAZ@users.noreply.github.com>
2024-02-29 00:22:02 +0500

    solve error

--- src/app/Svgs/GataClock.tsx
+++ src/app/Svgs/GataClock.tsx
@@ -10,62 +10,62 @@ const GataClock = () => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g clip-path="url(#clip0_143_443)">
+        <g clipPath="url(#clip0_143_443)">
           <path
             d="M26.5757 70.082L21.269 79.6571"
             stroke="black"
-            stroke-width="16"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M58.4246 70.082L63.7313 79.6571"
             stroke="black"
-            stroke-width="16"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M42.5001 74.3493C60.0886 74.3493 74.3469 60.091 74.3469 42.5025C74.3469 24.9141 60.0886 10.6558 42.5001 10.6558C24.9116 10.6558 10.6533 24.9141 10.6533 42.5025C10.6533 60.091 24.9116 74.3493 42.5001 74.3493Z"
             stroke="black"
-            stroke-width="16"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M11.7189 34.0131C9.27401 32.1805 7.40794 29.6833 6.3434 26.8192C5.27886 23.9552 5.06081 20.8454 5.71526 17.8608C6.36971 14.8763 7.86903 12.1431 10.0343 9.98727C12.1996 7.83148 14.9394 6.34419 17.9268 5.70286C20.9142 5.06153 24.023 5.29325 26.8823 6.37036C29.7416 7.44748 32.2307 9.32451 34.0525 11.7775"
             stroke="black"
-            stroke-width="16"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M42.5 10.6559L42.5001 5.34814"
             stroke="black"
-            stroke-width="16"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M42.5 26.5791V42.5024"
             stroke="black"
-            stroke-width="16"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M42.5 42.5024L53.7595 53.7619"
             stroke="black"
-            stroke-width="16"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
             d="M73.2813 34.0131C75.7262 32.1805 77.5923 29.6833 78.6568 26.8192C79.7214 23.9552 79.9394 20.8454 79.285 17.8608C78.6305 14.8763 77.1312 12.1431 74.9659 9.98727C72.8006 7.83148 70.0608 6.34419 67.0734 5.70286C64.086 5.06153 60.9772 5.29325 58.1179 6.37036C55.2586 7.44748 52.7695 9.32451 50.9478 11.7775"
             stroke="black"
-            stroke-width="16"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </g>
         <defs>
